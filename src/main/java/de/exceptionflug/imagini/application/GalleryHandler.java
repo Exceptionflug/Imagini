243cbec81b0a735f5e8b96d87fca41b1df5c9bee
Nico Britze <n.britze@exceptionflug.de>
2020-07-03 15:04:02 +0200

    Begin pagination

--- src/main/java/de/exceptionflug/imagini/application/GalleryHandler.java
+++ src/main/java/de/exceptionflug/imagini/application/GalleryHandler.java
@@ -1,5 +1,6 @@
 package de.exceptionflug.imagini.application;
 
+import com.google.common.collect.Ordering;
 import de.exceptionflug.imagini.ImaginiServer;
 import de.exceptionflug.imagini.config.Account;
 import de.exceptionflug.imagini.elements.GalleryDomElement;
@@ -9,12 +10,33 @@ import de.exceptionflug.moon.handler.PageHandler;
 import de.exceptionflug.moon.response.TextResponse;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.text.DateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 public class GalleryHandler implements PageHandler<TextResponse> {
+
+    private static final int ITEMS_PER_PAGE = 50;
 
     private static final String GALLERY_TEMPLATE = FileUtils.getFileContents(
             ImaginiServer.class.getResourceAsStream("/html/gallery_template.html")
     );
+
+    private static final Ordering<File> FILE_ORDERING = Ordering.from((o1, o2) -> {
+        try {
+            BasicFileAttributes attributes1 = Files.readAttributes(o1.toPath(), BasicFileAttributes.class);
+            BasicFileAttributes attributes2 = Files.readAttributes(o2.toPath(), BasicFileAttributes.class);
+            return Long.compare(attributes2.creationTime().toMillis(), attributes1.creationTime().toMillis());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    });
 
     private final ImaginiServer imaginiServer;
 
@@ -35,14 +57,42 @@ public class GalleryHandler implements PageHandler<TextResponse> {
         TextResponse out = new TextResponse(GALLERY_TEMPLATE, "text/html");
         StringBuilder stringBuilder = new StringBuilder();
 
+        int page = getPageNumber(request);
+        if(page < 1) {
+            page = 1;
+        }
+
         File dir = new File("content/"+account.getName());
-        for(File child : dir.listFiles()) {
+        List<File> files = FILE_ORDERING.sortedCopy(Arrays.asList(dir.listFiles()));
+
+        int startIndex = (page-1)*ITEMS_PER_PAGE;
+        int endIndex = Math.min(files.size(), startIndex + ITEMS_PER_PAGE - 1);
+        for (int i = startIndex; i < endIndex; i++) {
+            File child = files.get(i);
+
+            BasicFileAttributes basicFileAttributes = Files.readAttributes(child.toPath(), BasicFileAttributes.class);
             stringBuilder.append(new GalleryDomElement(account.getRedirectUrl()+child.getName(),
-                    account.getRedirectUrl()+child.getName(), "", child.getName()));
+                    account.getRedirectUrl()+child.getName(),
+                    DateFormat.getDateTimeInstance().format(new Date(basicFileAttributes.creationTime().toMillis())),
+                    child.getName()));
         }
 
         out.replace("%gallery%", stringBuilder.toString());
+        out.replace("%page%", Integer.toString(page));
+        out.replace("%pageCount%", Integer.toString((int) Math.ceil(files.size() / (double) ITEMS_PER_PAGE)));
         return out;
+    }
+
+    private int getPageNumber(Request request) {
+        String pageStr = request.getQueryParameter("page");
+        if(pageStr == null) {
+            return 1;
+        }
+        try {
+            return Integer.parseInt(pageStr);
+        } catch (NumberFormatException e) {
+            return 1;
+        }
     }
 
 }
